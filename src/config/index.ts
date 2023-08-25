ed97a2ed7460cd71c5103c7321f400232f738b9f
JP Rose <test@test.com>
2023-08-24 22:48:51 -0400

    2min pour la config auto

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ const config = {
     DEBUG_MODE: false,
     CORS_PROXY: "https://corsproxy.io/?",
     DEFAULT_PROFILE: "jp",
-    AUTOMATIC_TIMER: 60
+    AUTOMATIC_TIMER: 120
 }
 
 export default config;
